9e6de0bff0049caea58c72f9c1555e3922ec9a65
Agustin Chiappe Berrini <jnieve@gmail.com>
2019-07-05 06:56:14 -0700

    Moving solidity project

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate nom;
+
+mod llvm;
+mod parser;
+pub use llvm::*;
+pub use parser::Program;
+
+pub fn parse(_content: &str) -> Result<Program, Vec<String>> {
+    Err(Vec::new())
 }
